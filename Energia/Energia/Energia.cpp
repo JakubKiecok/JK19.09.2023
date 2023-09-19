a135187a4a1da93e8750ba114798ce5d9b85fb23
JakubKiecok <jkkiecok@gmail.com>
2023-09-19 10:37:05 +0200

    zajecia

---
+++ Energia/Energia/Energia.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int g = 10;
+float Em(float m, float v, float h);
+float Ek(float m, float v);
+float Ep(float m, float h);
+
+int main()
+{
+    float m, v, h;
+    cout << "Podaj mase = "; 
+    cin >> m;
+    cout << "Podaj prędkosc = ";
+    cin >> v;
+    cout << "Podaj wysokosc = ";
+    cin >> h;
+    cout << "Energia mechaniczna wynosi = " << Em(m, v, h) << endl;
+    cout << "Energia kinetyczna wynosi = " << Ek(m, v) << endl;
+    cout << "Energia potencjalna wynosi = " << Ep(m, h) << endl;
+}
+
+float Em(float m, float v, float h)
+{
+    return (m * pow(v, 2)) / 2 + m * h * g;
+}
+
+float Ek(float m, float v)
+{
+    return (m * pow(v, 2) / 2);
+}
+
+float Ep(float m, float h)
+{
+    return m * h * g;
+}
---
+++ KalkulatorVer2/KalkulatorVer2/KalkulatorVer2.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void opisKalkulatora();//deklaracja funkcji nic nie zwracającej
+float dodawanie(float l1, float l2);
+float odejmowanie(float l1, float l2);
+float mnozenie(float l1, float l2);
+void dzielenie(float l1, float l2);
+double pierwiastekliczba1(float l1);
+double pierwiastekliczba2(float l2);
+double potegowanie(float l1, float l2);
+int main()
+{
+    float liczba1, liczba2;
+    opisKalkulatora();
+    cout << "Podaj liczba1 = "; cin >> liczba1;
+    cout << "Podaj liczba2 = "; cin >> liczba2;
+    cout << "Dodawanie = " << dodawanie(liczba1, liczba2) << endl;
+    cout << "Odejmowanie = " << odejmowanie(liczba1, liczba2) << endl;
+    cout << "Mnozenie = " << mnozenie(liczba1, liczba2) << endl;
+    dzielenie(liczba1, liczba2);
+    cout << "Pierwiastek L1 = " << pierwiastekliczba1(liczba1) << endl;
+    cout << "Pierwiastek L2 = " << pierwiastekliczba2(liczba2) << endl;
+    cout << "Potęgowanie = " << potegowanie(liczba1, liczba2) << endl;
+    system("pause");
+}
+
+//definicja funkcji wczesniej zadeklarowanej
+void opisKalkulatora() {
+    cout << "Jakub Kiecok: Ver 2 Kalkulator" << endl;
+}
+
+float dodawanie(float l1, float l2)
+{
+    return l1 + l2;
+}
+
+float odejmowanie(float l1, float l2)
+{
+    return l1 - l2;
+}
+
+float mnozenie(float l1, float l2)
+{
+    return l1 * l2;
+}
+
+void dzielenie(float l1, float l2)
+{
+    if (l2 == 0) {
+        cout << "Nie dziel szlachcicu przez 0" << endl;
+    }
+    else
+    {
+        cout << "Dzielenie = " << l1 / l2 << endl;
+    }
+}
+
+double pierwiastekliczba1(float l1)
+{
+    if (l1 >= 0) return sqrt(l1);
+}
+
+double pierwiastekliczba2(float l2)
+{
+    if (l2 >= 0) return sqrt(l2);
+}
+
+double potegowanie(float l1, float l2)
+{
+    return pow(l1, l2);
+}
